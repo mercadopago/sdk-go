f3f7c9ff8b96acde5192e303ad3a27e4ce72a72c
gabs <gabriel.deandraderodrigues@mercadolivre.com>
2024-01-04 17:55:25 -0300

    Add comments

--- pkg/httpclient/request_options.go
+++ pkg/httpclient/request_options.go
@@ -9,6 +9,7 @@ type requestOptions struct {
 	CustomHeaders    http.Header
 }
 
+// RequestOption signature for client request configurable parameters.
 type RequestOption interface {
 	applyRequest(*requestOptions)
 }
@@ -17,6 +18,7 @@ type requestOptFunc func(opts *requestOptions)
 
 func (f requestOptFunc) applyRequest(o *requestOptions) { f(o) }
 
+// WithRequestRequester allow do request api call using received requester.
 func WithRequestRequester(r Requester) RequestOption {
 	return requestOptFunc(func(options *requestOptions) {
 		if r != nil {
@@ -25,6 +27,7 @@ func WithRequestRequester(r Requester) RequestOption {
 	})
 }
 
+// WithCustomHeaders set request headers, it will be send to api.
 func WithCustomHeaders(h http.Header) RequestOption {
 	return requestOptFunc(func(options *requestOptions) {
 		options.CustomHeaders = h
