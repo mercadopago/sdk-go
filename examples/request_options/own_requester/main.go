fb2f4a96841c0e6f061a61b4d7c79eee94473b67
gabs <gabriel.deandraderodrigues@mercadolivre.com>
2024-01-04 17:10:57 -0300

    Remove payments implementation

--- examples/request_options/own_requester/main.go
+++ examples/request_options/own_requester/main.go
@@ -1,12 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-
-	"github.com/mercadopago/sdk-go/pkg/httpclient"
-	"github.com/mercadopago/sdk-go/pkg/mp"
-	"github.com/mercadopago/sdk-go/pkg/payment"
 )
 
 type myRequester struct{}
@@ -16,33 +11,33 @@ func (*myRequester) Do(req *http.Request) (*http.Response, error) {
 	return nil, nil
 }
 
-func main() {
-	mp.SetAccessToken("TEST-640110472259637-071923-a761f639c4eb1f0835ff7611f3248628-793910800")
+// func main() {
+// 	mp.SetAccessToken("TEST-640110472259637-071923-a761f639c4eb1f0835ff7611f3248628-793910800")
 
-	pc := payment.NewClient()
+// 	pc := payment.NewClient()
 
-	request := payment.Request{
-		TransactionAmount: 1.5,
-		PaymentMethodID:   "pix",
-		Description:       "my payment",
-		Payer: &payment.PayerRequest{
-			Email: "gabs@testuser.com",
-		},
-	}
+// 	request := payment.Request{
+// 		TransactionAmount: 1.5,
+// 		PaymentMethodID:   "pix",
+// 		Description:       "my payment",
+// 		Payer: &payment.PayerRequest{
+// 			Email: "gabs@testuser.com",
+// 		},
+// 	}
 
-	// can be a http.Client from standard library:
-	// standardLibClient := &http.Client{}
-	// or can be a custom requester
-	myOwnRequester := &myRequester{}
+// 	// can be a http.Client from standard library:
+// 	// standardLibClient := &http.Client{}
+// 	// or can be a custom requester
+// 	myOwnRequester := &myRequester{}
 
-	res, err := pc.Create(
-		request,
-		httpclient.WithRequestRequester(myOwnRequester), // sdk will use that requester
-	)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+// 	res, err := pc.Create(
+// 		request,
+// 		httpclient.WithRequestRequester(myOwnRequester), // sdk will use that requester
+// 	)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
 
-	fmt.Println(res.ID)
-}
+// 	fmt.Println(res.ID)
+// }
