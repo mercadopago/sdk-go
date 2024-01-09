698231ad1469ca73f0447d6591df5f5aa2a5c4f7
gabs <gabriel.deandraderodrigues@mercadolivre.com>
2024-01-09 11:54:17 -0300

    Add custom header example

--- examples/request_options/custom_headers/main.go
+++ examples/request_options/custom_headers/main.go
@@ -1,30 +1,31 @@
 package main
 
-// func main() {
-// 	mp.SetAccessToken("TEST-640110472259637-071923-a761f639c4eb1f0835ff7611f3248628-793910800")
+import (
+	"fmt"
+	"net/http"
 
-// 	pc := payment.NewClient()
+	"github.com/mercadopago/sdk-go/pkg/api"
+	"github.com/mercadopago/sdk-go/pkg/api/paymentmethod"
+	"github.com/mercadopago/sdk-go/pkg/mp"
+)
 
-// 	request := payment.Request{
-// 		TransactionAmount: 1.5,
-// 		PaymentMethodID:   "pix",
-// 		Description:       "my payment",
-// 		Payer: &payment.PayerRequest{
-// 			Email: "gabs@testuser.com",
-// 		},
-// 	}
+func main() {
+	mp.SetAccessToken("TEST-640110472259637-071923-a761f639c4eb1f0835ff7611f3248628-793910800")
 
-// 	ch := http.Header{}
-// 	ch.Add("X-Idempotency-Key", "123999")
-// 	ch.Add("Some-Key", "some_value")
-// 	res, err := pc.Create(
-// 		request,
-// 		httpclient.WithCustomHeaders(ch), // http client will use these custom headers
-// 	)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		return
-// 	}
+	pmc := paymentmethod.NewClient()
 
-// 	fmt.Println(res.ID)
-// }
+	ch := http.Header{}
+	ch.Add("X-Idempotency-Key", "123999")
+	ch.Add("Some-Key", "some_value")
+	res, err := pmc.List(
+		api.WithCustomHeaders(ch), // http client will use these custom headers
+	)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, v := range res {
+		fmt.Println(v)
+	}
+}
