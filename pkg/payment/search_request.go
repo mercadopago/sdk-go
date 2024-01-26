75781202bd202a36a7933c428092de799daf801e
gabs <gabriel.deandraderodrigues@mercadolivre.com>
2024-01-26 09:59:12 -0300

    Adjust search

--- pkg/payment/search_request.go
+++ pkg/payment/search_request.go
@@ -1,40 +1,43 @@
 package payment
 
-// Filters is the filters to search for payments.
-type Filters struct {
-	// Sort is a field used to sort a list of payments.
-	// The sort can be done by the following attributes:
-	//	- "date_approved"
-	//	- "date_created"
-	//	- "date_last_updated"
-	//	- "id"
-	//	- "money_release_date"
-	Sort string
+import (
+	"net/url"
+	"strings"
+)
 
-	// Criteria is a field used to define the order of the result list.
-	// Can be "asc" or "desc".
-	Criteria string
+// SearchRequest is the request to search services.
+// Filters field can receive a lot of paramaters. For details, see:
+// https://www.mercadopago.com.br/developers/pt/reference/payments/_payments_search/get.
+type SearchRequest struct {
+	Limit  string
+	Offset string
 
-	// ExternalReference is an external reference of the payment.
-	// It can be, for example, a hashcode from the Central Bank, working as an identifier of the transaction origin.
-	ExternalReference string
+	Filters map[string]string
+}
 
-	// Range is a field used to define the range of the search.
-	// The Range can be related to the following attributes:
-	//	- "date_created"
-	//	- "date_last_updated"
-	//	- "date_approved"
-	//	- "money_release_date"
-	// If not informed, it uses "date_created" by default.
-	Range string
+// Parameters transforms SearchRequest into url params.
+func (s SearchRequest) Parameters() string {
+	params := url.Values{}
 
-	// BeginDate is a field used to define the start of the search interval.
-	// Its format can be a relative date - "NOW-XDAYS", "NOW-XMONTHS" - or an absolute date - ISO8601.
-	// If not informed, it uses "NOW-3MONTHS" by default.
-	BeginDate string
+	var limitKey, offsetKey bool
+	for k, v := range s.Filters {
+		params.Add(k, v)
 
-	// EndDate is a field used to define the end of the search interval.
-	// Its format can be a relative date - "NOW-XDAYS", "NOW-XMONTHS" - or an absolute date - ISO8601.
-	// If not informed, it uses "NOW" by default.
-	EndDate string
+		if strings.EqualFold(k, "limit") {
+			limitKey = true
+			continue
+		}
+		if strings.EqualFold(k, "offset") {
+			offsetKey = true
+		}
+	}
+
+	if !limitKey {
+		params.Add("limit", s.Limit)
+	}
+	if !offsetKey {
+		params.Add("offset", s.Offset)
+	}
+
+	return params.Encode()
 }
