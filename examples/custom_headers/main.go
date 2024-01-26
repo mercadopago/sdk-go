2e9317212247a3e099a37c09787d235e4e816047
gabs <gabriel.deandraderodrigues@mercadolivre.com>
2024-01-26 17:12:48 -0300

    Change credential scheme and export Requester interface

--- examples/custom_headers/main.go
+++ examples/custom_headers/main.go
@@ -5,19 +5,20 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/mercadopago/sdk-go/pkg/credential"
+	"github.com/mercadopago/sdk-go/pkg/config"
 	"github.com/mercadopago/sdk-go/pkg/header"
 	"github.com/mercadopago/sdk-go/pkg/paymentmethod"
 )
 
 func main() {
-	cdt, err := credential.New("TEST-640110472259637-071923-a761f639c4eb1f0835ff7611f3248628-793910800")
+	at := "TEST-640110472259637-071923-a761f639c4eb1f0835ff7611f3248628-793910800"
+	c, err := config.New(at)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	pmc := paymentmethod.NewClient(cdt)
+	client := paymentmethod.NewClient(c)
 
 	headers := http.Header{}
 	headers.Add("X-Idempotency-Key", "123999")
@@ -27,7 +28,7 @@ func main() {
 	// This will return a child context decorated with your custom headers.
 	// They will be forwarded wherever this context is used in any of the APIs.
 	ctx = header.Context(ctx, headers)
-	res, err := pmc.List(
+	res, err := client.List(
 		ctx,
 	)
 
--- pkg/internal/httpclient/gateway.go
+++ pkg/internal/httpclient/gateway.go
@@ -7,8 +7,9 @@ import (
 	"net/http"
 
 	"github.com/google/uuid"
-	"github.com/mercadopago/sdk-go/pkg/credential"
+	"github.com/mercadopago/sdk-go/pkg/config"
 	"github.com/mercadopago/sdk-go/pkg/header"
+	"github.com/mercadopago/sdk-go/pkg/option"
 )
 
 const (
@@ -19,22 +20,22 @@ const (
 	idempotencyHeader   = "X-Idempotency-Key"
 )
 
-func Send(ctx context.Context, cdt *credential.Credential, requester Requester, req *http.Request) ([]byte, error) {
+func Send(ctx context.Context, c *config.Config, req *http.Request) ([]byte, error) {
 	for k, v := range header.Headers(ctx) {
 		canonicalKey := http.CanonicalHeaderKey(k)
 		req.Header[canonicalKey] = v
 	}
 
-	req.Header.Set(authorizationHeader, "Bearer "+string(*cdt))
+	req.Header.Set(authorizationHeader, "Bearer "+c.AccessToken)
 	req.Header.Set(productIDHeader, productID)
 	if _, ok := req.Header[idempotencyHeader]; !ok {
 		req.Header.Set(idempotencyHeader, uuid.New().String())
 	}
 
-	return send(ctx, requester, req)
+	return send(ctx, c.HTTPClient, req)
 }
 
-func send(ctx context.Context, requester Requester, req *http.Request) ([]byte, error) {
+func send(ctx context.Context, requester option.Requester, req *http.Request) ([]byte, error) {
 	res, err := requester.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("transport level error: %w", err)
