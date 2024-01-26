4f80b4e876c267e2e1059ee9d2680935757c12d3
gabs <gabriel.deandraderodrigues@mercadolivre.com>
2024-01-26 11:21:43 -0300

    Change custom headers context key

--- pkg/header/header.go
+++ pkg/header/header.go
@@ -5,13 +5,17 @@ import (
 	"net/http"
 )
 
-type customHeadersCtxKey struct{}
+type customHeadersCtxKey string
+
+const (
+	customHeadersKey = customHeadersCtxKey("customHeaders")
+)
 
 func Context(ctx context.Context, customHeaders http.Header) context.Context {
-	return context.WithValue(ctx, customHeadersCtxKey{}, customHeaders)
+	return context.WithValue(ctx, customHeadersKey, customHeaders)
 }
 
 func Headers(ctx context.Context) http.Header {
-	value, _ := ctx.Value(customHeadersCtxKey{}).(http.Header)
+	value, _ := ctx.Value(customHeadersKey).(http.Header)
 	return value
 }
