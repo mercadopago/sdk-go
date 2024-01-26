186d9c40331fd79e3b69da494bb5ad3184dc6b97
gabs <gabriel.deandraderodrigues@mercadolivre.com>
2024-01-26 19:33:34 -0300

    Fix tests

--- pkg/paymentmethod/payment_method_test.go
+++ pkg/paymentmethod/payment_method_test.go
@@ -10,22 +10,18 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/mercadopago/sdk-go/pkg/credential"
+	"github.com/mercadopago/sdk-go/pkg/config"
 	"github.com/mercadopago/sdk-go/pkg/internal/httpclient"
-	"github.com/mercadopago/sdk-go/pkg/option"
 )
 
 var (
-	cdt, _ = credential.New("any")
-
 	listResponseJSON, _ = os.Open("../../resources/mocks/payment_method_list.json")
 	listResponse, _     = io.ReadAll(listResponseJSON)
 )
 
 func TestList(t *testing.T) {
 	type fields struct {
-		credential *credential.Credential
-		config     *option.ClientOptions
+		config *config.Config
 	}
 	type args struct {
 		ctx context.Context
@@ -40,8 +36,7 @@ func TestList(t *testing.T) {
 		{
 			name: "should_return_error_when_creating_request",
 			fields: fields{
-				credential: cdt,
-				config:     option.ApplyClientOptions(),
+				config: nil,
 			},
 			args: args{
 				ctx: nil,
@@ -52,16 +47,13 @@ func TestList(t *testing.T) {
 		{
 			name: "should_return_error_when_send_request",
 			fields: fields{
-				credential: cdt,
-				config: option.ApplyClientOptions(
-					option.WithCustomClient(
-						&httpclient.Mock{
-							DoMock: func(req *http.Request) (*http.Response, error) {
-								return nil, fmt.Errorf("some error")
-							},
+				config: &config.Config{
+					HTTPClient: &httpclient.Mock{
+						DoMock: func(req *http.Request) (*http.Response, error) {
+							return nil, fmt.Errorf("some error")
 						},
-					),
-				),
+					},
+				},
 			},
 			args: args{
 				ctx: context.Background(),
@@ -72,20 +64,17 @@ func TestList(t *testing.T) {
 		{
 			name: "should_return_error_unmarshal_response",
 			fields: fields{
-				credential: cdt,
-				config: option.ApplyClientOptions(
-					option.WithCustomClient(
-						&httpclient.Mock{
-							DoMock: func(req *http.Request) (*http.Response, error) {
-								stringReader := strings.NewReader("invalid json")
-								stringReadCloser := io.NopCloser(stringReader)
-								return &http.Response{
-									Body: stringReadCloser,
-								}, nil
-							},
+				config: &config.Config{
+					HTTPClient: &httpclient.Mock{
+						DoMock: func(req *http.Request) (*http.Response, error) {
+							stringReader := strings.NewReader("invalid json")
+							stringReadCloser := io.NopCloser(stringReader)
+							return &http.Response{
+								Body: stringReadCloser,
+							}, nil
 						},
-					),
-				),
+					},
+				},
 			},
 			args: args{
 				ctx: context.Background(),
@@ -96,20 +85,17 @@ func TestList(t *testing.T) {
 		{
 			name: "should_return_formatted_response",
 			fields: fields{
-				credential: cdt,
-				config: option.ApplyClientOptions(
-					option.WithCustomClient(
-						&httpclient.Mock{
-							DoMock: func(req *http.Request) (*http.Response, error) {
-								stringReader := strings.NewReader(string(listResponse))
-								stringReadCloser := io.NopCloser(stringReader)
-								return &http.Response{
-									Body: stringReadCloser,
-								}, nil
-							},
+				config: &config.Config{
+					HTTPClient: &httpclient.Mock{
+						DoMock: func(req *http.Request) (*http.Response, error) {
+							stringReader := strings.NewReader(string(listResponse))
+							stringReadCloser := io.NopCloser(stringReader)
+							return &http.Response{
+								Body: stringReadCloser,
+							}, nil
 						},
-					),
-				),
+					},
+				},
 			},
 			args: args{
 				ctx: context.Background(),
@@ -183,8 +169,7 @@ func TestList(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			c := &client{
-				credential: tt.fields.credential,
-				config:     tt.fields.config,
+				config: tt.fields.config,
 			}
 			got, err := c.List(tt.args.ctx)
 			gotErr := ""
