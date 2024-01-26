a72f39bfff80f4355dacf52087baa6af264f19b7
gabs <gabriel.deandraderodrigues@mercadolivre.com>
2024-01-26 19:40:00 -0300

    Fix tests

--- test/integration/payment_method_test.go
+++ test/integration/payment_method_test.go
@@ -5,18 +5,18 @@ import (
 	"os"
 	"testing"
 
-	"github.com/mercadopago/sdk-go/pkg/credential"
+	"github.com/mercadopago/sdk-go/pkg/config"
 	"github.com/mercadopago/sdk-go/pkg/paymentmethod"
 )
 
 func TestList(t *testing.T) {
 	t.Run("should_list_payment_methods", func(t *testing.T) {
-		cdt, err := credential.New(os.Getenv("at"))
+		c, err := config.New(os.Getenv("at"))
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		pmc := paymentmethod.NewClient(cdt)
+		pmc := paymentmethod.NewClient(c)
 		res, err := pmc.List(context.Background())
 
 		if res == nil {
