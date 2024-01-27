919bd39b63a3a7ee0e720556f2a71246c25cf64b
gabs <gabriel.deandraderodrigues@mercadolivre.com>
2024-01-26 21:12:04 -0300

    Add capture amount test

--- pkg/payment/payment_test.go
+++ pkg/payment/payment_test.go
@@ -29,6 +29,9 @@ var (
 
 	captureResponseJSON, _ = os.Open("../../resources/mocks/payment/capture_response.json")
 	captureResponse, _     = io.ReadAll(captureResponseJSON)
+
+	captureAmountResponseJSON, _ = os.Open("../../resources/mocks/payment/capture_amount_response.json")
+	captureAmountResponse, _     = io.ReadAll(captureAmountResponseJSON)
 )
 
 func TestCreate(t *testing.T) {
@@ -611,6 +614,118 @@ func TestCapture(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("client.Capture() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCaptureAmount(t *testing.T) {
+	type fields struct {
+		config *config.Config
+	}
+	type args struct {
+		ctx    context.Context
+		id     int64
+		amount float64
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *Response
+		wantErr string
+	}{
+		{
+			name: "should_fail_to_create_request",
+			fields: fields{
+				config: nil,
+			},
+			args: args{
+				ctx: nil,
+			},
+			want:    nil,
+			wantErr: "error creating request: net/http: nil Context",
+		},
+		{
+			name: "should_fail_to_send_request",
+			fields: fields{
+				config: &config.Config{
+					HTTPClient: &httpclient.Mock{
+						DoMock: func(req *http.Request) (*http.Response, error) {
+							return nil, fmt.Errorf("some error")
+						},
+					},
+				},
+			},
+			args: args{
+				ctx: context.Background(),
+			},
+			want:    nil,
+			wantErr: "transport level error: some error",
+		},
+		{
+			name: "should_fail_to_unmarshaling_response",
+			fields: fields{
+				config: &config.Config{
+					HTTPClient: &httpclient.Mock{
+						DoMock: func(req *http.Request) (*http.Response, error) {
+							stringReader := strings.NewReader("invalid json")
+							stringReadCloser := io.NopCloser(stringReader)
+							return &http.Response{
+								Body: stringReadCloser,
+							}, nil
+						},
+					},
+				},
+			},
+			args: args{
+				ctx: context.Background(),
+			},
+			want:    nil,
+			wantErr: "error unmarshaling response: invalid character 'i' looking for beginning of value",
+		},
+		{
+			name: "should_return_formatted_response",
+			fields: fields{
+				config: &config.Config{
+					HTTPClient: &httpclient.Mock{
+						DoMock: func(req *http.Request) (*http.Response, error) {
+							stringReader := strings.NewReader(string(captureAmountResponse))
+							stringReadCloser := io.NopCloser(stringReader)
+							return &http.Response{
+								Body: stringReadCloser,
+							}, nil
+						},
+					},
+				},
+			},
+			args: args{
+				ctx: context.Background(),
+			},
+			want: &Response{
+				ID:           123,
+				Status:       "approved",
+				StatusDetail: "accredited",
+			},
+			wantErr: "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &client{
+				config: tt.fields.config,
+			}
+			got, err := c.CaptureAmount(tt.args.ctx, tt.args.id, tt.args.amount)
+			gotErr := ""
+			if err != nil {
+				gotErr = err.Error()
+			}
+
+			if gotErr != tt.wantErr {
+				t.Errorf("client.CaptureAmount() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("client.CaptureAmount() = %v, want %v", got, tt.want)
 			}
 		})
 	}
