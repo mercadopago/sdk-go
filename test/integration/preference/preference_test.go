4a8160c97cf41ddf1b731cb8f1c3d83dac7fc612
Bruna Campos <bruna.camposf@mercadolivre.com>
2024-01-29 14:35:38 -0300

    add integration test to preference

---
+++ test/integration/preference/preference_test.go
@@ -0,0 +1,131 @@
+package integration
+
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/mercadopago/sdk-go/pkg/config"
+	"github.com/mercadopago/sdk-go/pkg/preference"
+)
+
+func TestCreate(t *testing.T) {
+	t.Run("should_create_preference", func(t *testing.T) {
+		c, err := config.New(os.Getenv("at"))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		dto := preference.Request{
+			Items: []preference.PreferenceItemRequest{
+				{
+					ID:          "123",
+					Title:       "Title",
+					UnitPrice: 	100,
+					Quantity:    1,
+					Description: "Description",
+				},
+			},
+		}
+
+		pmc := preference.NewClient(c)
+		res, err := pmc.Create(context.Background(), dto)
+
+		if res == nil {
+			t.Error("res can't be nil")
+		}
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+	})
+
+	t.Run("should_get_preference", func(t *testing.T) {
+		c, err := config.New(os.Getenv("at"))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		pmc := preference.NewClient(c)
+		res, err := pmc.Get(context.Background(), "1273205088-5b386915-144d-4db9-ac96-f683867289d2")
+
+		if res == nil {
+			t.Error("res can't be nil")
+		}
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+	})
+
+	t.Run("should_update_preference", func(t *testing.T) {
+		c, err := config.New(os.Getenv("at"))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		create := preference.Request{
+			Items: []preference.PreferenceItemRequest{
+				{
+					ID:          "123",
+					Title:       "Title",
+					UnitPrice: 	100,
+					Quantity:    1,
+					Description: "Description",
+				},
+			},
+		}
+		pmc := preference.NewClient(c)
+		res, err := pmc.Create(context.Background(), create)
+		if res == nil {
+			t.Error("res can't be nil")
+		}
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+
+		update := preference.Request{
+			Items: []preference.PreferenceItemRequest{
+				{
+					ID:          "123",
+					Title:       "Title",
+					UnitPrice: 	100,
+					Quantity:    1,
+					Description: "Description",
+				},
+			},
+		}
+
+		if res != nil {
+			res, err = pmc.Update(context.Background(), res.ID, update)
+
+			if res == nil {
+				t.Error("res can't be nil")
+			}
+		}
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+	})
+
+	t.Run("should_search_preference", func(t *testing.T) {
+		c, err := config.New(os.Getenv("at"))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		filters := preference.SearchRequest{
+			Limit: 10,
+			Offset: 10,
+		}
+
+		pmc := preference.NewClient(c)
+		res, err := pmc.Search(context.Background(), filters)
+
+		if res == nil {
+			t.Error("res can't be nil")
+		}
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+	})
+}
