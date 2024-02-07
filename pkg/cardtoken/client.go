a35ebf8abe5fdeaf0bf014deec64eef48c4c11c3
meliguilhermefernandes <guilherme.jfernandes@mercadolivre.com>
2024-02-07 14:57:15 -0300

    Card token - refacting client

--- pkg/cardtoken/client.go
+++ pkg/cardtoken/client.go
@@ -2,9 +2,7 @@ package cardtoken
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"net/http"
 	"strings"
 
 	"github.com/mercadopago/sdk-go/pkg/config"
@@ -31,44 +29,17 @@ func NewClient(c *config.Config) Client {
 }
 
 func (c *client) Get(ctx context.Context, id string) (*Response, error) {
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, urlGet, nil)
+	res, err := httpclient.Get[Response](ctx, c.cfg, strings.Replace(urlGet, "{id}", id, 1))
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error get card token: %w", err)
 	}
-
-	r, err := httpclient.Send(ctx, c.cfg, req)
-	if err != nil {
-		return nil, err
-	}
-
-	var res *Response
-	if err := json.Unmarshal(r, res); err != nil {
-		return nil, fmt.Errorf("error unmarshaling card token response: %w", err)
-	}
-
 	return res, nil
 }
 
 func (c *client) Create(ctx context.Context, request Request) (*Response, error) {
-	body, err := json.Marshal(&request)
+	res, err := httpclient.Post[Response](ctx, c.cfg, urlPost, request)
 	if err != nil {
-		return nil, fmt.Errorf("error marshaling card token request: %w", err)
+		return nil, fmt.Errorf("error create card token: %w", err)
 	}
-
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, urlPost, strings.NewReader(string(body)))
-	if err != nil {
-		return nil, fmt.Errorf("error creating card token request: %w", err)
-	}
-
-	r, err := httpclient.Send(ctx, c.cfg, req)
-	if err != nil {
-		return nil, err
-	}
-
-	var res *Response
-	if err := json.Unmarshal(r, res); err != nil {
-		return nil, fmt.Errorf("error unmarshaling card token response: %w", err)
-	}
-
 	return res, nil
 }
