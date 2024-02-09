fd4201bd165ee236ca0a43feb9512f7d1b2dd263
meliguilhermefernandes <guilherme.jfernandes@mercadolivre.com>
2024-02-09 09:59:53 -0300

    Card token - code quality

--- pkg/cardtoken/request.go
+++ pkg/cardtoken/request.go
@@ -1,20 +1,20 @@
 package cardtoken
 
 type Request struct {
-	SiteID          string      `json:"site_id"`
-	CardNumber      string      `json:"card_number"`
-	ExpirationYear  string      `json:"expiration_year"`
-	ExpirationMonth string      `json:"expiration_month"`
-	SecurityCode    string      `json:"security_code"`
-	Cardholder      *Cardholder `json:"cardholder,omitempty"`
+	SiteID          string      `json:"site_id,omitempty"`
+	CardNumber      string      `json:"card_number,omitempty"`
+	ExpirationYear  string      `json:"expiration_year,omitempty"`
+	ExpirationMonth string      `json:"expiration_month,omitempty"`
+	SecurityCode    string      `json:"security_code,omitempty"`
+	Cardholder      *Cardholder `json:"cardholder,omitempty,omitempty"`
 }
 
 type Cardholder struct {
 	Identification *Identification `json:"identification,omitempty"`
-	Name           string          `json:"name"`
+	Name           string          `json:"name,omitempty"`
 }
 
 type Identification struct {
-	Number string `json:"number"`
-	Type   string `json:"type"`
+	Number string `json:"number,omitempty"`
+	Type   string `json:"type,omitempty"`
 }
