b47050c79a6b5b9bef319e4d63bae74bfd59b447
Rhames Lima <rhames.lima@mercadolivre.com>
2024-02-14 12:37:09 -0300

    Add Identification Types Client

---
+++ pkg/identificationtypes/response.go
@@ -0,0 +1,9 @@
+package identificationtypes
+
+type Response struct {
+	ID        string `json:"id"`
+	Name      string `json:"name"`
+	Type      string `json:"type"`
+	MinLength int    `json:"min_length"`
+	MaxLength int    `json:"max_length"`
+}
