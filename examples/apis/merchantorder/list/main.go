687bd7f4e636f31960a3cc19889fd146b5cc8537
edmarSoaress <edmar.slima@mercadolivre.com>
2024-02-16 17:58:57 -0300

    add merchant order

---
+++ examples/apis/merchantorder/list/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/mercadopago/sdk-go/pkg/config"
+	"github.com/mercadopago/sdk-go/pkg/refund"
+)
+
+func main() {
+	accessToken := "{{ACCESS_TOKEN}}"
+
+	cfg, err := config.New(accessToken)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	refundClient := refund.NewClient(cfg)
+
+	var paymentID int64 = 12233344
+
+	refunds, err := refundClient.List(context.Background(), paymentID)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for _, ref := range refunds {
+		fmt.Println(ref)
+	}
+}
---
+++ examples/apis/merchantorder/total_refund/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/mercadopago/sdk-go/pkg/config"
+	"github.com/mercadopago/sdk-go/pkg/payment"
+	"github.com/mercadopago/sdk-go/pkg/refund"
+)
+
+func main() {
+	accessToken := "{{ACCESS_TOKEN}}"
+
+	cfg, err := config.New(accessToken)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Create payment.
+	req := payment.Request{
+		TransactionAmount: 105.1,
+		PaymentMethodID:   "master",
+		Payer: &payment.PayerRequest{
+			Email: "{{EMAIL}}",
+		},
+		Token:        "{{TOKEN}}",
+		Installments: 1,
+		Capture:      false,
+	}
+
+	client := payment.NewClient(cfg)
+	pay, err := client.Create(context.Background(), req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	refundClient := refund.NewClient(cfg)
+	ref, err := refundClient.Create(context.Background(), pay.ID)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(ref)
+}
