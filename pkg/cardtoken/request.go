e3cc79152c17a9f394a30cb9deaf59fc14d7d7ee
gabs <gabriel.deandraderodrigues@mercadolivre.com>
2024-02-22 09:33:08 -0300

    Change cardtoken package documentation

--- pkg/cardtoken/request.go
+++ pkg/cardtoken/request.go
@@ -1,22 +1,25 @@
 package cardtoken
 
+// Request contains every field accepted by Card Tokens API.
 type Request struct {
-	SiteID          string `json:"site_id,omitempty"`
-	CardNumber      string `json:"card_number,omitempty"`
-	ExpirationYear  string `json:"expiration_year,omitempty"`
-	ExpirationMonth string `json:"expiration_month,omitempty"`
-	SecurityCode    string `json:"security_code,omitempty"`
+	SiteID          string `json:"site_id,omitempty"`          // represents the country: MLA (Argentina), MLB (Brazil), MLC (Chile), MLM (Mexico), MLU (Uruguay), MCO (Colombia), MPE (Peru)
+	CardNumber      string `json:"card_number,omitempty"`      // card number used to generate card token
+	ExpirationYear  string `json:"expiration_year,omitempty"`  // expiration year used to generate card token
+	ExpirationMonth string `json:"expiration_month,omitempty"` // expiration month used to generate card token
+	SecurityCode    string `json:"security_code,omitempty"`    // security code used to generate card token
 
-	Cardholder *Cardholder `json:"cardholder,omitempty"`
+	Cardholder *CardholderRequest `json:"cardholder,omitempty"` // contains cardholder data used to generate card token
 }
 
-type Cardholder struct {
-	Name string `json:"name,omitempty"`
+// CardholderRequest contains cardholder data used to generate card token.
+type CardholderRequest struct {
+	Name string `json:"name,omitempty"` // cardholder name
 
-	Identification *Identification `json:"identification,omitempty"`
+	Identification *IdentificationRequest `json:"identification,omitempty"` // cardholder identification
 }
 
-type Identification struct {
-	Number string `json:"number,omitempty"`
-	Type   string `json:"type,omitempty"`
+// IdentificationRequest contains cardholder identification data used to generate card token.
+type IdentificationRequest struct {
+	Number string `json:"number,omitempty"` // idenfication number, its format can change depending on the country
+	Type   string `json:"type,omitempty"`   // idenfication type, its value can change depending on the country
 }
