e1d812626aeba4ef79764b4fce838860ca11416b
Bruna Campos <bruna.camposf@mercadolivre.com>
2024-02-29 19:09:28 -0300

    adjustments requested - organizing types

--- pkg/invoice/response.go
+++ pkg/invoice/response.go
@@ -2,28 +2,30 @@ package invoice
 
 import "time"
 
+// Response is the response from the Invoice API.
 type Response struct {
-	CurrencyID        string          `json:"currency_id"`
-	DateCreated       *time.Time      `json:"date_created"`
-	DebitDate         *time.Time      `json:"debit_date"`
-	ExternalReference string          `json:"external_reference"`
-	ID                int             `json:"id"`
-	LastModified      *time.Time      `json:"last_modified"`
-	NextRetryDate     *time.Time      `json:"next_retry_date"`
-	Payment           PaymentResponse `json:"payment"`
-	PaymentMethodID   string          `json:"payment_method_id"`
-	PreApprovalID     string          `json:"preapproval_id"`
-	Reason            string          `json:"reason"`
-	RetryAttempt      int             `json:"retry_attempt"`
-	Status            string          `json:"status"`
-	Summarized        string          `json:"summarized"`
-	TransactionAmount float64         `json:"transaction_amount"`
-	Type              string          `json:"type"`
+	CurrencyID        string  `json:"currency_id"`
+	ExternalReference string  `json:"external_reference"`
+	PaymentMethodID   string  `json:"payment_method_id"`
+	PreApprovalID     string  `json:"preapproval_id"`
+	Reason            string  `json:"reason"`
+	Status            string  `json:"status"`
+	Summarized        string  `json:"summarized"`
+	Type              string  `json:"type"`
+	ID                int     `json:"id"`
+	RetryAttempt      int     `json:"retry_attempt"`
+	TransactionAmount float64 `json:"transaction_amount"`
+
+	DateCreated   *time.Time      `json:"date_created"`
+	DebitDate     *time.Time      `json:"debit_date"`
+	LastModified  *time.Time      `json:"last_modified"`
+	NextRetryDate *time.Time      `json:"next_retry_date"`
+	Payment       PaymentResponse `json:"payment"`
 }
 
 // PaymentResponse contains information about payment.
 type PaymentResponse struct {
-	ID           int    `json:"id"`
 	Status       string `json:"status"`
 	StatusDetail string `json:"status_detail"`
+	ID           int    `json:"id"`
 }
