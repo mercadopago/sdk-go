29ba0c9cc8738b6d1c9d9cfa00909d314a56756d
Rhames Lima <rhames.lima@mercadolivre.com>
2024-03-01 11:10:07 -0300

    Minor adjustments

--- pkg/customercard/request.go
+++ pkg/customercard/request.go
@@ -1,7 +1,7 @@
 package customercard
 
 type Request struct {
-	Issuer IssuerRequest `json:"issuer,omitempty"`
+	Issuer *IssuerRequest `json:"issuer,omitempty"`
 
 	Token           string `json:"token,omitempty"`
 	PaymentMethodID string `json:"payment_method_id,omitempty"`
