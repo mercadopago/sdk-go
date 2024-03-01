aea02512e82b6e3ebaef31a871d739ea1cc16967
gabs <gabriel.deandraderodrigues@mercadolivre.com>
2024-03-01 16:19:33 -0300

    Remove useless tests

--- pkg/customer/client_test.go
+++ pkg/customer/client_test.go
@@ -43,17 +43,6 @@ func TestCreate(t *testing.T) {
 		wantErr string
 	}{
 		{
-			name: "should_return_error_when_creating_request",
-			fields: fields{
-				config: nil,
-			},
-			args: args{
-				ctx: nil,
-			},
-			want:    nil,
-			wantErr: "error creating request: net/http: nil Context",
-		},
-		{
 			name: "should_return_response",
 			fields: fields{
 				config: &config.Config{
