75fd48d29b58b05d3fdbe3c52d6ad4eac27a363d
gabs <gabriel.deandraderodrigues@mercadolivre.com>
2024-03-04 18:44:15 -0300

    Switch credentials

--- examples/apis/customer/search/main.go
+++ examples/apis/customer/search/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	accessToken := "TEST-4718610619866357-092020-f30ef41ea2a9e7ad0fa7bc101b5508af-751574177"
+	accessToken := "{{ACCESS_TOKEN}}"
 
 	cfg, err := config.New(accessToken)
 	if err != nil {
@@ -19,7 +19,7 @@ func main() {
 
 	req := customer.SearchRequest{
 		Filters: map[string]string{
-			"EMAIL": "akdsokdoakdoasakdoas@testuser.com",
+			"EMAIL": "{{EMAIL}}",
 		},
 	}
 
