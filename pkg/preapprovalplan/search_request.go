096127b6eb11e795f27615d0ebf07e8c697b8b00
Bruna Campos <bruna.camposf@mercadolivre.com>
2024-03-06 17:51:03 -0300

    Feature/preapproval plan (#28)
    * add pre approval plan - implementation, unit test, integration tests, examples
    
    * adjustments to maintain standard
    
    * adjustments made to maintain standard
    
    * adjustments made to maintain standard
    
    * adjustment in the name of struct and pointers
    
    * adjustments requested in code review
    
    * remove return in a test
    
    * add a return in a test
    
    * add a return in a test
    
    * remove a return in a test
    
    * remove wrong comment

---
+++ pkg/preapprovalplan/search_request.go
@@ -0,0 +1,40 @@
+package preapprovalplan
+
+import "net/url"
+
+// SearchRequest contains filters accepted in search.
+// Filters field can receive a lot of parameters. For details, see:
+// https://www.mercadopago.com/developers/en/reference/subscriptions/_preapproval_plan_id/get
+type SearchRequest struct {
+	Filters map[string]string
+
+	Limit  string
+	Offset string
+}
+
+// Parameters transforms SearchRequest into url params.
+func (s SearchRequest) Parameters() string {
+	params := url.Values{}
+
+	for k, v := range s.Filters {
+		params.Add(k, v)
+	}
+
+	if _, ok := s.Filters["limit"]; !ok {
+		limit := "30"
+		if s.Limit != "" {
+			limit = s.Limit
+		}
+		params.Add("limit", limit)
+	}
+
+	if _, ok := s.Filters["offset"]; !ok {
+		offset := "0"
+		if s.Offset != "" {
+			offset = s.Offset
+		}
+		params.Add("offset", offset)
+	}
+
+	return params.Encode()
+}
