090485ee0214009df0b0512cf8902415ae2f652a
Bruna Campos <bruna.camposf@mercadolivre.com>
2024-03-12 10:55:23 -0300

    Feature/preapproval (#32)
    * implementation, unit test, integration test and example
    
    * refactoring to adapt to merge
    
    * refactor type at integration test
    
    * edit comment
    
    * remove blank line
    
    * add omitempy to FreeTrialRequest
    
    * refactor field name
    
    * remove omitempty
    
    * rename variable name
    
    * rename variable name
    
    * rename variable name
    
    * refactor comment
    
    * refactor preapproval search IT
    
    * refactor searchResponse

---
+++ examples/apis/preapproval/update/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/mercadopago/sdk-go/pkg/config"
+	"github.com/mercadopago/sdk-go/pkg/preapproval"
+)
+
+func main() {
+	cfg, err := config.New("{{ACCESS_TOKEN}}")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	req := preapproval.Request{
+		AutoRecurring: &preapproval.AutoRecurringRequest{
+			Frequency:         1,
+			FrequencyType:     "months",
+			TransactionAmount: 100,
+			CurrencyID:        "BRL",
+		},
+		BackURL:           "https://www.yoursite.com",
+		ExternalReference: "Ref-123",
+		PayerEmail:        "test_user_28355466@testuser.com",
+		Reason:            "Yoga Class",
+	}
+
+	client := preapproval.NewClient(cfg)
+	result, err := client.Create(context.Background(), req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	update := preapproval.UpdateRequest{
+		AutoRecurring: &preapproval.AutoRecurringRequest{
+			Frequency:         1,
+			FrequencyType:     "months",
+			TransactionAmount: 100,
+			CurrencyID:        "BRL",
+		},
+		BackURL:           "https://www.yoursite.com",
+		ExternalReference: "Ref-123",
+		Reason:            "Yoga Class",
+	}
+
+	result, err = client.Update(context.Background(), update, result.ID)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(result)
+}
---
+++ pkg/preapproval/search_request.go
@@ -0,0 +1,33 @@
+package preapproval
+
+import (
+	"strconv"
+	"strings"
+)
+
+// SearchRequest contains filters accepted in search.
+// Filters field can receive a lot of parameters. For details, see:
+// https://www.mercadopago.com/developers/en/reference/subscriptions/_preapproval_search/get
+type SearchRequest struct {
+	Filters map[string]string
+
+	Limit  int
+	Offset int
+}
+
+// GetParams creates map to build query parameters. Keys will be changed to lower case.
+func (sr SearchRequest) GetParams() map[string]string {
+	params := map[string]string{}
+	for k, v := range sr.Filters {
+		key := strings.ToLower(k)
+		params[key] = v
+	}
+
+	if sr.Limit == 0 {
+		sr.Limit = 30
+	}
+	params["limit"] = strconv.Itoa(sr.Limit)
+	params["offset"] = strconv.Itoa(sr.Offset)
+
+	return params
+}
