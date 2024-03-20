21a2dd9f7f2d215189c45bdc4b10d6e0d666b9f2
Bruna Campos <bruna.camposf@mercadolivre.com>
2024-03-20 16:58:46 -0300

    Update pkg/paymentmethod/response.go
    Co-authored-by: gdeandradero <gabriel.deandraderodrigues@mercadolivre.com>

--- pkg/paymentmethod/response.go
+++ pkg/paymentmethod/response.go
@@ -1,6 +1,6 @@
 package paymentmethod
 
-// Response represents a list of all available payment methods in detail.
+// Response represents a detailed payment method.
 type Response struct {
 	Settings              []SettingsResponse             `json:"settings"`
 	FinancialInstitutions []FinancialInstitutionResponse `json:"financial_institutions"`
