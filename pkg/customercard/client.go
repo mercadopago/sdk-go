072489ee8852a9d7d50448de4a24a6d5f19267c1
gabs <gabriel.deandraderodrigues@mercadolivre.com>
2024-03-21 18:22:07 -0300

    Improve customercard doc

--- pkg/customercard/client.go
+++ pkg/customercard/client.go
@@ -21,29 +21,29 @@ const (
 
 // Client contains the methods to interact with the Customer Cards API.
 type Client interface {
-	// Create a new customer card.
-	// It is a post request to the endpoint: https://api.mercadopago.com/v1/customer/{customer_id}/cards
-	// Reference: https://www.mercadopago.com/developers/en/reference/cards/_customers_customer_id_cards/post
+	// Create a new card to the customer.
+	// It is a post request to the endpoint: https://api.mercadopago.com/v1/customer/{customer_id}/cards.
+	// Reference: https://www.mercadopago.com/developers/en/reference/cards/_customers_customer_id_cards/post.
 	Create(ctx context.Context, customerID string, request Request) (*Response, error)
 
-	// Get a customer card by id.
-	// It is a get request to the endpoint: https://api.mercadopago.com/v1/customer/{customer_id}/cards/{card_id}
-	// Reference: https://www.mercadopago.com/developers/en/reference/cards/_customers_customer_id_cards_id/get
+	// Get a customer card by customer id and card id.
+	// It is a get request to the endpoint: https://api.mercadopago.com/v1/customer/{customer_id}/cards/{card_id}.
+	// Reference: https://www.mercadopago.com/developers/en/reference/cards/_customers_customer_id_cards_id/get.
 	Get(ctx context.Context, customerID, cardID string) (*Response, error)
 
-	// Update a customer card by id.
-	// It is a put request to the endpoint: https://api.mercadopago.com/v1/customer/{customer_id}/cards/{card_id}
-	// Reference: https://www.mercadopago.com/developers/en/reference/cards/_customers_customer_id_cards_id/put
+	// Update a customer card by customer id and card id.
+	// It is a put request to the endpoint: https://api.mercadopago.com/v1/customer/{customer_id}/cards/{card_id}.
+	// Reference: https://www.mercadopago.com/developers/en/reference/cards/_customers_customer_id_cards_id/put.
 	Update(ctx context.Context, customerID, cardID string, request Request) (*Response, error)
 
-	// Delete deletes a customer card by id.
-	// It is a delete request to the endpoint: https://api.mercadopago.com/v1/customer/{customer_id}/cards/{card_id}
-	// Reference: https://www.mercadopago.com/developers/en/reference/cards/_customers_customer_id_cards_id/delete
+	// Delete a customer card by customer id and card id.
+	// It is a delete request to the endpoint: https://api.mercadopago.com/v1/customer/{customer_id}/cards/{card_id}.
+	// Reference: https://www.mercadopago.com/developers/en/reference/cards/_customers_customer_id_cards_id/delete.
 	Delete(ctx context.Context, customerID, cardID string) (*Response, error)
 
-	// List all customer cards.
-	// It is a get request to the endpoint: https://api.mercadopago.com/v1/customer/{customer_id}/cards
-	// Reference: https://www.mercadopago.com/developers/en/reference/cards/_customers_customer_id_cards/get
+	// List all customer's cards by id.
+	// It is a get request to the endpoint: https://api.mercadopago.com/v1/customer/{customer_id}/cards.
+	// Reference: https://www.mercadopago.com/developers/en/reference/cards/_customers_customer_id_cards/get.
 	List(ctx context.Context, customerID string) ([]Response, error)
 }
 
