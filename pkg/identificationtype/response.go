7b49ecd1458e7c162f6386a30e7b43415b6b8fe7
gabs <gabriel.deandraderodrigues@mercadolivre.com>
2024-03-21 17:50:38 -0300

    Add identification types doc

--- pkg/identificationtype/response.go
+++ pkg/identificationtype/response.go
@@ -1,9 +1,9 @@
 package identificationtype
 
 type Response struct {
-	ID        string `json:"id"`
-	Name      string `json:"name"`
-	Type      string `json:"type"`
-	MinLength int    `json:"min_length"`
-	MaxLength int    `json:"max_length"`
+	ID        string `json:"id"`         // identification type's id
+	Name      string `json:"name"`       // identification type's name
+	Type      string `json:"type"`       // identification type's type
+	MinLength int    `json:"min_length"` // identification type's min_length
+	MaxLength int    `json:"max_length"` // identification type's max_length
 }
