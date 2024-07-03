bd071990cb2f2a4a2ff5b8d0c1e7062fe2fa905d
Danielen Cestari Nunes <danielen.cestari@mercadolivre.com>
2024-07-03 16:19:11 -0300

    update exemple

--- examples/apis/preference/create/main.go
+++ examples/apis/preference/create/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	cfg, err := config.New("APP_USR-3031066189562927-042217-7204e46c8ca7b09cee4327a6e38f8c0d-831921084")
+	cfg, err := config.New("{{ACCESS_TOKEN}}")
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -20,26 +20,9 @@ func main() {
 			{
 				ID:          "123",
 				Title:       "Title",
-				UnitPrice:   75,
+				UnitPrice:   100,
 				Quantity:    1,
 				Description: "Description",
-			},
-		},
-		PaymentMethods: &preference.PaymentMethodsRequest{
-			DefaultPaymentMethodID: "visa",
-		},
-		Payer: &preference.PayerRequest{
-			Name:    "tyuio",
-			Surname: "gfhjkl",
-			Email:   "test_user_61213998@testuser.com",
-			Identification: &preference.IdentificationRequest{
-				Type:   "CPF",
-				Number: "19119119100",
-			},
-			Address: &preference.AddressRequest{
-				ZipCode:      "88054000",
-				StreetName:   "Rua PREFERENCE",
-				StreetNumber: "12345",
 			},
 		},
 	}
