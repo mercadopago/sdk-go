88d3d3652336d3a40198ae0f79e1e3a5a37c930b
Danielen Cestari Nunes <danielen.cestari@mercadolivre.com>
2025-02-11 16:25:01 -0300

    save progress

--- pkg/order/client.go
+++ pkg/order/client.go
@@ -2,14 +2,17 @@ package order
 
 import (
 	"context"
-	"net/http"
-
 	"github.com/mercadopago/sdk-go/pkg/config"
 	"github.com/mercadopago/sdk-go/pkg/internal/httpclient"
+	"net/http"
 )
 
 const (
-	urlBase = "https://api.mercadopago.com/v1/orders"
+	urlBase           = "https://api.mercadopago.com/v1/orders"
+	urlWithOrderID    = urlBase + "/" + "{orderID}"
+	urlTransaction    = urlWithOrderID + "/transactions"
+	urlProcess        = urlWithOrderID + "/process"
+	urlPutTransaction = urlTransaction + "{transactionID}"
 )
 
 // Client contains the methods to interact with the Order API.
@@ -18,6 +21,10 @@ type Client interface {
 	// It is a post request to the endpoint: https://api.mercadopago.com/v1/orders
 	// Reference: https://www.mercadopago.com.br/developers/pt/reference/order/online-payments/create/post
 	Create(ctx context.Context, request Request) (*Response, error)
+	Get(ctx context.Context, orderID string) (*Response, error)
+	CreateTransaction(ctx context.Context, orderID string, request TransactionRequest) (*TransactionResponse, error)
+	Process(ctx context.Context, orderID string) (*Response, error)
+	UpdateTransaction(ctx context.Context, orderID string, transactionID string, request TransactionRequest) (*TransactionResponse, error)
 }
 
 // client is the implementation of Client.
@@ -40,6 +47,82 @@ func (c *client) Create(ctx context.Context, request Request) (*Response, error)
 	}
 
 	resource, err := httpclient.DoRequest[*Response](ctx, c.cfg, requestData)
+	if err != nil {
+		return nil, err
+	}
+
+	return resource, nil
+}
+
+func (c *client) Get(ctx context.Context, orderID string) (*Response, error) {
+	pathParams := map[string]string{
+		"orderID": orderID,
+	}
+	requestData := httpclient.RequestData{
+		Method:     http.MethodGet,
+		URL:        urlWithOrderID,
+		PathParams: pathParams,
+	}
+
+	resource, err := httpclient.DoRequest[*Response](ctx, c.cfg, requestData)
+	if err != nil {
+		return nil, err
+	}
+
+	return resource, nil
+}
+
+func (c *client) Process(ctx context.Context, orderID string) (*Response, error) {
+	pathParams := map[string]string{
+		"orderID": orderID,
+	}
+	requestData := httpclient.RequestData{
+		Method:     http.MethodPost,
+		URL:        urlProcess,
+		PathParams: pathParams,
+	}
+
+	resource, err := httpclient.DoRequest[*Response](ctx, c.cfg, requestData)
+	if err != nil {
+		return nil, err
+	}
+
+	return resource, nil
+}
+
+func (c *client) CreateTransaction(ctx context.Context, orderID string, request TransactionRequest) (*TransactionResponse, error) {
+	pathParams := map[string]string{
+		"orderID": orderID,
+	}
+	requestData := httpclient.RequestData{
+		Body:       request,
+		Method:     http.MethodPost,
+		URL:        urlTransaction,
+		PathParams: pathParams,
+	}
+
+	resource, err := httpclient.DoRequest[*TransactionResponse](ctx, c.cfg, requestData)
+	if err != nil {
+		return nil, err
+	}
+
+	return resource, nil
+
+}
+
+func (c *client) UpdateTransaction(ctx context.Context, orderID string, transactionID string, request TransactionRequest) (*TransactionResponse, error) {
+	pathParams := map[string]string{
+		"orderID":       orderID,
+		"transactionID": transactionID,
+	}
+	requestData := httpclient.RequestData{
+		Body:       request,
+		Method:     http.MethodPut,
+		URL:        urlPutTransaction,
+		PathParams: pathParams,
+	}
+
+	resource, err := httpclient.DoRequest[*TransactionResponse](ctx, c.cfg, requestData)
 	if err != nil {
 		return nil, err
 	}
--- test/integration/order/order_test.go
+++ test/integration/order/order_test.go
@@ -57,3 +57,54 @@ func TestOrder(t *testing.T) {
 		}
 	})
 }
+
+func TestGetOrder(t *testing.T) {
+	t.Run("should_create_and_get_order", func(t *testing.T) {
+		ctx := context.Background()
+		token, err := test.GenerateCardToken(ctx, cardTokenClient)
+		if err != nil {
+			t.Error("fail to generate card token", err)
+		}
+
+		request := order.Request{
+			Type:              "online",
+			TotalAmount:       "1000.00",
+			ExternalReference: "ext_ref_12345",
+			Transactions: order.TransactionRequest{
+				Payments: []order.PaymentRequest{
+					{
+						Amount: "1000.00",
+						PaymentMethod: order.PaymentMethodRequest{
+							ID:           "master",
+							Token:        token,
+							Type:         "credit_card",
+							Installments: 1,
+						},
+					},
+				},
+			},
+			Payer: order.PayerRequest{
+				Email: fmt.Sprintf("test_user_%s@testuser.com", uuid.New().String()[:7]),
+			},
+		}
+
+		resource, err := orderClient.Create(ctx, request)
+		if resource == nil || resource.ID == "" {
+			t.Error("order can't be nil")
+		}
+
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+
+		resource, err = orderClient.Get(ctx, resource.ID)
+		if resource == nil || resource.ID == "" {
+			t.Error("order can't be nil")
+		}
+
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+
+	})
+}
