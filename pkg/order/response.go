2210dcd6422e87acb088ae50533b4c8a88384ee0
andreagostinho-meli <andre.agostinho@mercadolivre.com>
2025-03-31 13:45:02 -0300

    enhancement: add omitempty

--- pkg/order/response.go
+++ pkg/order/response.go
@@ -3,18 +3,18 @@ package order
 // API version: d0494f1c-8d81-4c76-ae1d-0c65bb8ef6de
 
 type Response struct {
-	ID                  string                  `json:"id"`
-	Type                string                  `json:"type"`
-	ExternalReference   string                  `json:"external_reference"`
-	CountryCode         string                  `json:"country_code"`
-	Status              string                  `json:"status"`
-	StatusDetail        string                  `json:"status_detail"`
-	CaptureMode         string                  `json:"capture_mode"`
+	ID                  string                  `json:"id,omitempty"`
+	Type                string                  `json:"type,omitempty"`
+	ExternalReference   string                  `json:"external_reference,omitempty"`
+	CountryCode         string                  `json:"country_code,omitempty"`
+	Status              string                  `json:"status,omitempty"`
+	StatusDetail        string                  `json:"status_detail,omitempty"`
+	CaptureMode         string                  `json:"capture_mode,omitempty"`
 	UserID              string                  `json:"user_id,omitempty"`
 	ClientToken         string                  `json:"client_token,omitempty"`
-	TotalAmount         string                  `json:"total_amount"`
+	TotalAmount         string                  `json:"total_amount,omitempty"`
 	TotalPaidAmount     string                  `json:"total_paid_amount,omitempty"`
-	ProcessingMode      string                  `json:"processing_mode"`
+	ProcessingMode      string                  `json:"processing_mode,omitempty"`
 	Description         string                  `json:"description,omitempty"`
 	Marketplace         string                  `json:"marketplace,omitempty"`
 	MarketplaceFee      string                  `json:"marketplace_fee,omitempty"`
@@ -22,29 +22,29 @@ type Response struct {
 	ExpirationTime      string                  `json:"expiration_time,omitempty"`
 	CreatedDate         string                  `json:"created_date,omitempty"`
 	LastUpdatedDate     string                  `json:"last_updated_date,omitempty"`
-	Transactions        TransactionResponse     `json:"transactions"`
+	Transactions        TransactionResponse     `json:"transactions,omitempty"`
 	Items               []ItemsResponse         `json:"items,omitempty"`
 	IntegrationData     IntegrationDataResponse `json:"integration_data,omitempty"`
 	Config              ConfigResponse          `json:"config,omitempty"`
-	Payer               PayerResponse           `json:"payer"`
+	Payer               PayerResponse           `json:"payer,omitempty"`
 }
 
 type TransactionResponse struct {
-	Payments []PaymentResponse `json:"payments"`
+	Payments []PaymentResponse `json:"payments,omitempty"`
 	Refunds  []RefundResponse  `json:"refunds,omitempty"`
 }
 
 type PaymentResponse struct {
-	ID                string                   `json:"id"`
-	ReferenceID       string                   `json:"reference_id"`
-	Status            string                   `json:"status"`
+	ID                string                   `json:"id,omitempty"`
+	ReferenceID       string                   `json:"reference_id,omitempty"`
+	Status            string                   `json:"status,omitempty"`
 	StatusDetail      string                   `json:"status_detail,omitempty"`
-	Amount            string                   `json:"amount"`
+	Amount            string                   `json:"amount,omitempty"`
 	PaidAmount        string                   `json:"paid_amount,omitempty"`
 	DateOfExpiration  string                   `json:"date_of_expiration,omitempty"`
 	ExpirationTime    string                   `json:"expiration_time,omitempty"`
 	AttemptNumber     int                      `json:"attempt_number,omitempty"`
-	PaymentMethod     PaymentMethodResponse    `json:"payment_method"`
+	PaymentMethod     PaymentMethodResponse    `json:"payment_method,omitempty"`
 	AutomaticPayments AutomaticPaymentResponse `json:"automatic_payments,omitempty"`
 	StoredCredential  StoredCredentialResponse `json:"stored_credential,omitempty"`
 	SubscriptionData  SubscriptionDataResponse `json:"subscription_data,omitempty"`
@@ -70,70 +70,70 @@ type PaymentMethodResponse struct {
 }
 
 type AutomaticPaymentResponse struct {
-	PaymentProfileID string `json:"payment_profile_id"`
-	ScheduleDate     string `json:"schedule_date"`
-	DueDate          string `json:"due_date"`
-	Retries          int    `json:"retries"`
+	PaymentProfileID string `json:"payment_profile_id,omitempty"`
+	ScheduleDate     string `json:"schedule_date,omitempty"`
+	DueDate          string `json:"due_date,omitempty"`
+	Retries          int    `json:"retries,omitempty"`
 }
 
 type StoredCredentialResponse struct {
-	PaymentInitiator   string `json:"payment_initiator"`
-	Reason             string `json:"reason"`
-	StorePaymentMethod bool   `json:"store_payment_method"`
-	FirstPayment       bool   `json:"first_payment"`
+	PaymentInitiator   string `json:"payment_initiator,omitempty"`
+	Reason             string `json:"reason,omitempty"`
+	StorePaymentMethod bool   `json:"store_payment_method,omitempty"`
+	FirstPayment       bool   `json:"first_payment,omitempty"`
 }
 
 type SubscriptionDataResponse struct {
-	InvoiceID            string                       `json:"invoice_id"`
-	BillingDate          string                       `json:"billing_date"`
-	SubscriptionSequence SubscriptionSequenceResponse `json:"subscription_sequence"`
-	InvoicePeriod        InvoicePeriodResponse        `json:"invoice_period"`
+	InvoiceID            string                       `json:"invoice_id,omitempty"`
+	BillingDate          string                       `json:"billing_date,omitempty"`
+	SubscriptionSequence SubscriptionSequenceResponse `json:"subscription_sequence,omitempty"`
+	InvoicePeriod        InvoicePeriodResponse        `json:"invoice_period,omitempty"`
 }
 
 type SubscriptionSequenceResponse struct {
-	Number int `json:"number"`
-	Total  int `json:"total"`
+	Number int `json:"number,omitempty"`
+	Total  int `json:"total,omitempty"`
 }
 
 type InvoicePeriodResponse struct {
-	Type   string `json:"type"`
-	Period int    `json:"period"`
+	Type   string `json:"type,omitempty"`
+	Period int    `json:"period,omitempty"`
 }
 
 type RefundResponse struct {
-	ID            string          `json:"id"`
-	TransactionID string          `json:"transaction_id"`
-	ReferenceID   string          `json:"reference_id"`
-	Status        string          `json:"status"`
-	Amount        string          `json:"amount"`
+	ID            string          `json:"id,omitempty"`
+	TransactionID string          `json:"transaction_id,omitempty"`
+	ReferenceID   string          `json:"reference_id,omitempty"`
+	Status        string          `json:"status,omitempty"`
+	Amount        string          `json:"amount,omitempty"`
 	Items         []ItemsResponse `json:"items,omitempty"`
 }
 
 type RefundReferenceResponse struct {
-	ID       string `json:"id"`
-	SourceID string `json:"source_id"`
+	ID       string `json:"id,omitempty"`
+	SourceID string `json:"source_id,omitempty"`
 }
 
 type ItemsResponse struct {
-	Title        string `json:"title"`
-	UnitPrice    string `json:"unit_price"`
-	ExternalCode string `json:"external_code"`
-	Description  string `json:"description"`
-	CategoryID   string `json:"category_id"`
-	PictureURL   string `json:"picture_url"`
-	Quantity     int    `json:"quantity"`
+	Title        string `json:"title,omitempty"`
+	UnitPrice    string `json:"unit_price,omitempty"`
+	ExternalCode string `json:"external_code,omitempty"`
+	Description  string `json:"description,omitempty"`
+	CategoryID   string `json:"category_id,omitempty"`
+	PictureURL   string `json:"picture_url,omitempty"`
+	Quantity     int    `json:"quantity,omitempty"`
 }
 
 type IntegrationDataResponse struct {
-	CorporationID string          `json:"corporation_id"`
-	ApplicationID string          `json:"application_id"`
-	IntegratorID  string          `json:"integrator_id"`
-	PlatformID    string          `json:"platform_id"`
-	Sponsor       SponsorResponse `json:"sponsor"`
+	CorporationID string          `json:"corporation_id,omitempty"`
+	ApplicationID string          `json:"application_id,omitempty"`
+	IntegratorID  string          `json:"integrator_id,omitempty"`
+	PlatformID    string          `json:"platform_id,omitempty"`
+	Sponsor       SponsorResponse `json:"sponsor,omitempty"`
 }
 
 type SponsorResponse struct {
-	ID string `json:"id"`
+	ID string `json:"id,omitempty"`
 }
 
 type ConfigResponse struct {
@@ -163,7 +163,7 @@ type DifferentialPricingResponse struct {
 }
 
 type PayerResponse struct {
-	CustomerID string `json:"customer_id"`
+	CustomerID string `json:"customer_id,omitempty"`
 }
 
 type AttemptResponse struct {
