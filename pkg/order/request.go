022187a1251db59fe2cac4787fc2a4a9ff6955c5
rsosantos <anderson.rsantos@mercadolivre.com>
2025-05-14 15:19:54 -0300

    Add industry fields

--- pkg/order/request.go
+++ pkg/order/request.go
@@ -1,84 +1,92 @@
 package order
 
-// API version: d0494f1c-8d81-4c76-ae1d-0c65bb8ef6de
-
 type Request struct {
-	Type                string                `json:"type,omitempty"`
-	TotalAmount         string                `json:"total_amount,omitempty"`
-	ExternalReference   string                `json:"external_reference,omitempty"`
-	CaptureMode         string                `json:"capture_mode,omitempty"`
-	ProcessingMode      string                `json:"processing_mode,omitempty"`
-	Description         string                `json:"description,omitempty"`
-	Marketplace         string                `json:"marketplace,omitempty"`
-	MarketPlaceFee      string                `json:"marketplace_fee,omitempty"`
-	ExpirationTime      string                `json:"expiration_time,omitempty"`
-	CheckoutAvailableAt string                `json:"checkout_available_at,omitempty"`
-	Transactions        *TransactionRequest   `json:"transactions,omitempty"`
-	Payer               *PayerRequest         `json:"payer,omitempty"`
-	Items               []ItemsRequest        `json:"items,omitempty"`
-	Config              *ConfigRequest        `json:"config,omitempty"`
-	Address             []AddressRequest      `json:"adresses,omitempty"`
-	AdditionalInfo      AdditionalInfoRequest `json:"additional_info,omitempty"`
+	Type                string                   `json:"type,omitempty"`
+	TotalAmount         string                   `json:"total_amount,omitempty"`
+	ExternalReference   string                   `json:"external_reference,omitempty"`
+	CaptureMode         string                   `json:"capture_mode,omitempty"`
+	ProcessingMode      string                   `json:"processing_mode,omitempty"`
+	Description         string                   `json:"description,omitempty"`
+	Marketplace         string                   `json:"marketplace,omitempty"`
+	MarketPlaceFee      string                   `json:"marketplace_fee,omitempty"`
+	ExpirationTime      string                   `json:"expiration_time,omitempty"`
+	CheckoutAvailableAt string                   `json:"checkout_available_at,omitempty"`
+	Transactions        *TransactionRequest      `json:"transactions,omitempty"`
+	Payer               *PayerRequest            `json:"payer,omitempty"`
+	Items               []ItemsRequest           `json:"items,omitempty"`
+	Config              *ConfigRequest           `json:"config,omitempty"`
+	Address             []AddressRequest         `json:"adresses,omitempty"`
+	AdditionalInfo      *AdditionalInfoRequest   `json:"additional_info,omitempty"`
+	Shipment            *ShipmentRequest         `json:"shipment,omitempty"`
+	TravelPassengers    []TravelPassengerRequest `json:"travel.passengers,omitempty"`
 }
 
+type TravelPassengerRequest struct {
+	FirstName      string                 `json:"first_name,omitempty"`
+	LastName       string                 `json:"last_name,omitempty"`
+	Identification *IdentificationRequest `json:"identification,omitempty"`
+}
+
+type TravelRouterequest struct {
+	Departure         string `json:"departure,omitempty"`
+	Destination       string `json:"destination,omitempty"`
+	DepartureDateTime string `json:"departure_date_time,omitempty"`
+	ArrivalDateTime   string `json:"arrival_date_time,omitempty"`
+	Company           string `json:"company,omitempty"`
+}
 type AdditionalInfoRequest struct {
-	Payer    *AdditionalInfoPayer    `json:"payer,omitempty"`
-	Shipment *AdditionalInfoShipment `json:"shipment,omitempty"`
-	Platform *AdditionalInfoPlatform `json:"platform,omitempty"`
-	Travel   *AdditionalInfoTravel   `json:"travel,omitempty"`
+	PayerAuthenticationType            string              `json:"payer.authentication_type,omitempty"`
+	PayerRegistrationDate              string              `json:"payer.registration_date,omitempty"`
+	PayerIsPrimeUser                   bool                `json:"payer.is_prime_user,omitempty"`
+	PayerIsFirstPurchaseOnLine         bool                `json:"payer.is_first_purchase_onLine,omitempty"`
+	PayerLastPurchase                  string              `json:"payer.last_purchase,omitempty"`
+	ShipmentExpress                    bool                `json:"shipment.express,omitempty"`
+	ShipmentLocalPickup                bool                `json:"shipment.local_pickup,omitempty"`
+	PlatFormShipmentDeliveryPromise    string              `json:"platform.shipment.delivery_promise,omitempty"`
+	PlatFormShipmentDropShipping       string              `json:"platform.shipment.drop_shipping,omitempty"`
+	PlatformShipmentSafety             string              `json:"platform.shipment.safety,omitempty"`
+	PlatformShipmentTrackingCode       string              `json:"platform.shipment.tracking.code,omitempty"`
+	PlatformShipmentTrackingStatus     string              `json:"platform.shipment.tracking.status,omitempty"`
+	PlatformShipmentWithdrawn          bool                `json:"platform.shipment.withdrawn,omitempty"`
+	PlatformSellerId                   string              `json:"platform.seller.id,omitempty"`
+	PlatformSellerName                 string              `json:"platform.seller.name,omitempty"`
+	PlatformSellerEmail                string              `json:"platform.seller.email,omitempty"`
+	PlatformSellerStatus               string              `json:"platform.seller.status,omitempty"`
+	PlatformSellerReferralUrl          string              `json:"platform.seller.referral_url,omitempty"`
+	PlatformSellerRegistrationDate     string              `json:"platform.seller.registration_date,omitempty"`
+	PlatformSellerHiredPlan            string              `json:"platform.seller.hired_plan,omitempty"`
+	PlatformSellerBusinessType         string              `json:"platform.seller.business_type,omitempty"`
+	PlatformSellerAddressZipCode       string              `json:"platform.seller.address.zip_code,omitempty"`
+	PlatformSellerAddressStreetName    string              `json:"platform.seller.address.street_name,omitempty"`
+	PlatformSellerAddressStreetNumber  string              `json:"platform.seller.address.street_number,omitempty"`
+	PlatformSellerAddressCity          string              `json:"platform.seller.address.city,omitempty"`
+	PlatformSellerAddressState         string              `json:"platform.seller.address.state,omitempty"`
+	PlatformSellerAddressComplement    string              `json:"platform.seller.address.complement,omitempty"`
+	PlatformSellerAddressCountry       string              `json:"platform.seller.address.country,omitempty"`
+	PlatformSellerIdentificationType   string              `json:"platform.seller.identification.type,omitempty"`
+	PlatformSellerIdentificationNumber string              `json:"platform.seller.identification.number,omitempty"`
+	PlatformSellerPhoneAreaCode        string              `json:"platform.seller.phone.area_code,omitempty"`
+	PlatformSellerPhoneNumber          string              `json:"platform.seller.phone.number,omitempty"`
+	PlatformAuthentication             string              `json:"platform.authentication,omitempty"`
+	TravelPassengers                   *PayerRequest       `json:"travel.passengers,omitempty"`
+	TravelRoutes                       *TravelRouterequest `json:"travel.routes,omitempty"`
 }
 
-type AdditionalInfoPayer struct {
-	AuthenticationType    string        `json:"authentication_type,omitempty"`
-	RegistrationDate      string        `json:"registration_date,omitempty"`
-	IsPrimeUser           bool          `json:"is_prime_user,omitempty"`
-	IsFirstPurchaseOnline bool          `json:"is_first_purchase_online,omitempty"`
-	LastPurchase          string        `json:"last_purchase,omitempty"`
-	Address               *PayerAddress `json:"address,omitempty"`
+type PayerAddressRequest struct {
+	Zipcode      string `json:"zipcode,omitempty"`
+	StreetName   string `json:"street_name,omitempty"`
+	StreetNumber string `json:"street_number,omitempty"`
+	Neighborhood string `json:"neighborhood,omitempty"`
+	City         string `json:"city,omitempty"`
+	State        string `json:"state,omitempty"`
+	Complement   string `json:"complement,omitempty"`
+	Country      string `json:"country,omitempty"`
 }
 
-type AdditionalInfoShipment struct {
-	Express     bool `json:"express,omitempty"`
-	LocalPickup bool `json:"local_pickup,omitempty"`
+type ShipmentRequest struct {
+	Address *AddressRequest `json:"address,omitempty"`
 }
 
-type AdditionalInfoPlatform struct {
-	Shipment       *AdditionalInfoPlatformShipment `json:"shipment,omitempty"`
-	Seller         *AdditionalInfoPlatformSeller   `json:"seller,omitempty"`
-	Authentication string                          `json:"authentication,omitempty"`
-}
-
-type AdditionalInfoPlatformShipment struct {
-	DeliveryPromise string                          `json:"delivery_promise,omitempty"`
-	DropShipping    bool                            `json:"drop_shipping,omitempty"`
-	Safety          string                          `json:"safety,omitempty"`
-	Tracking        *AdditionalInfoPlatformTracking `json:"tracking,omitempty"`
-	Withdrawn       bool                            `json:"withdrawn,omitempty"`
-}
-
-type AdditionalInfoPlatformTracking struct {
-	Code   string `json:"code,omitempty"`
-	Status string `json:"status,omitempty"`
-}
-
-type AdditionalInfoPlatformSeller struct {
-	ID                 string                 `json:"id,omitempty"`
-	Name               string                 `json:"name,omitempty"`
-	Email              string                 `json:"email,omitempty"`
-	Status             string                 `json:"status,omitempty"`
-	ReferralURL        string                 `json:"referral_url,omitempty"`
-	RegistrationDate   string                 `json:"registration_date,omitempty"`
-	HiredPlan          string                 `json:"hired_plan,omitempty"`
-	BusinessType       string                 `json:"business_type,omitempty"`
-	Address            *AddressRequest        `json:"address,omitempty"`
-	IdentificationType *IdentificationRequest `json:"identification,omitempty"`
-	Phone              *PhoneRequest          `json:"phone,omitempty"`
-}
-
-type AdditionalInfoTravel struct {
-	Passengers []string `json:"passengers,omitempty"`
-	Routes     []string `json:"routes,omitempty"`
-}
 type TransactionRequest struct {
 	Payments []PaymentRequest `json:"payments,omitempty"`
 }
@@ -138,7 +146,7 @@ type PayerRequest struct {
 	CustomerID     string                 `json:"customer_id,omitempty"`
 	Identification *IdentificationRequest `json:"identification,omitempty"`
 	Phone          *PhoneRequest          `json:"phone,omitempty"`
-	Address        *PayerAddress          `json:"address,omitempty"`
+	Address        *PayerAddressRequest   `json:"address,omitempty"`
 }
 type PayerAddress struct {
 	ZipCode      string `json:"zip_code,omitempty"`
@@ -173,7 +181,7 @@ type AddressRequest struct {
 type ItemsRequest struct {
 	Title        string `json:"title,omitempty"`
 	Type         string `json:"type,omitempty"`
-	Warranty     string `json:"warranty,omitempty"`
+	Warranty     bool   `json:"warranty,omitempty"`
 	Event_date   string `json:"event_date,omitempty"`
 	UnitPrice    string `json:"unit_price,omitempty"`
 	ExternalCode string `json:"external_code,omitempty"`
