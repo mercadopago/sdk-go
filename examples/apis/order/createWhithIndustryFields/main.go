cd4924b043efd833c5a5787b05487f0fb5ceefd1
rsosantos <anderson.rsantos@mercadolivre.com>
2025-05-14 15:19:54 -0300

    Add industry fields

--- examples/apis/order/createWhithIndustryFields/main.go
+++ examples/apis/order/createWhithIndustryFields/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	accessToken := "APP_USR-874202490252970-100714-e890db6519b0dceb4ef24ef41ed816e4-2021490138"
+	accessToken := "<ACCESS_TOKEN>"
 	c, err := config.New(accessToken)
 	if err != nil {
 		fmt.Println(err)
@@ -29,7 +29,7 @@ func main() {
 					Amount: "1000.00",
 					PaymentMethod: &order.PaymentMethodRequest{
 						ID:           "master",
-						Token:        "6176fb9fe529922e788c3db01d9a8738",
+						Token:        "<CARD_TOKEN>",
 						Type:         "credit_card",
 						Installments: 1,
 					},
@@ -38,43 +38,89 @@ func main() {
 		},
 
 		Payer: &order.PayerRequest{
-			Email:     "anderson@testuser.com",
-			FirstName: "Anderson",
-			LastName:  "TestUser",
+			Email:     "<EMAIL_ADDRESS>",
+			FirstName: "<FIRST_NAME>",
+			LastName:  "<LAST_NAME>",
 			Identification: &order.IdentificationRequest{
-				Type:   "CPF",
-				Number: "000000000",
+				Type:   "<CPF>",
+				Number: "<NUMBER>",
 			},
 			Phone: &order.PhoneRequest{
 				AreaCode: "55",
 				Number:   "9881107889",
 			},
-			Address: &order.PayerAddress{
-				StreetName:   "TestUser",
-				StreetNumber: "125",
-				ZipCode:      "123",
-				Neighborhood: "TestUser",
-				City:         "TestUser",
-				State:        "TestUser",
-				Complement:   "TestUser",
+			Address: &order.PayerAddressRequest{
+				StreetName:   "<STREET_NAME>",
+				StreetNumber: "<STREET_NUMBER>",
+				Neighborhood: "	BAIRRO",
+				City:         "CIDADE",
+				State:        "SP",
+				Complement:   "303",
 			},
 		},
-
 		Items: []order.ItemsRequest{
 			{
 				ExternalCode: "ext_ref_1234",
-				Title:        "Test",
+				Title:        "Passagem Para SP",
 				Description:  "Test",
 				PictureURL:   "http://picture.testuser.com",
 				CategoryID:   "test",
 				Quantity:     1,
 				Type:         "travel",
 				UnitPrice:    "1000.00",
-				Warranty:     "true",
-				Event_date:   "2020-01-01",
+				Warranty:     true,
+				Event_date:   "2023-10-10T00:00:00Z",
 			},
 		},
+		Shipment: &order.ShipmentRequest{
+			Address: &order.AddressRequest{
+				StreetName:   "<STREET_NAME>",
+				StreetNumber: "129",
+				ZipCode:      "06233903",
+				Neighborhood: "BAIRRO",
+				City:         "Osasco",
+				State:        "SP",
+				Complement:   "2",
+			},
+		},
+
+		AdditionalInfo: &order.AdditionalInfoRequest{
+			PayerAuthenticationType:            "MOBILE",
+			PayerRegistrationDate:              "2020-08-06T09:25:04.000-03:00",
+			PayerIsPrimeUser:                   true,
+			PayerIsFirstPurchaseOnLine:         false,
+			PayerLastPurchase:                  "2020-08-06T09:25:04.000-03:00",
+			ShipmentExpress:                    true,
+			ShipmentLocalPickup:                true,
+			PlatFormShipmentDeliveryPromise:    "2024-12-31T23:59:59Z",
+			PlatFormShipmentDropShipping:       "string",
+			PlatformShipmentSafety:             "string",
+			PlatformShipmentTrackingCode:       "1234",
+			PlatformShipmentTrackingStatus:     "Em rota",
+			PlatformShipmentWithdrawn:          true,
+			PlatformSellerId:                   "123456",
+			PlatformSellerName:                 "NAME",
+			PlatformSellerEmail:                "<EMAIL>",
+			PlatformSellerStatus:               "Active",
+			PlatformSellerReferralUrl:          "https://www.testuser.com/seller/123456",
+			PlatformSellerRegistrationDate:     "2020-01-01T00:00:00.000-03:00",
+			PlatformSellerHiredPlan:            "Premium",
+			PlatformSellerBusinessType:         "E-commerce",
+			PlatformSellerAddressZipCode:       "123456",
+			PlatformSellerAddressStreetName:    "NAME",
+			PlatformSellerAddressStreetNumber:  "125",
+			PlatformSellerAddressCity:          "São Paulo",
+			PlatformSellerAddressState:         "SP",
+			PlatformSellerAddressComplement:    "101",
+			PlatformSellerAddressCountry:       "Brasil",
+			PlatformSellerIdentificationType:   "CNPJ",
+			PlatformSellerIdentificationNumber: "<NUMBER>",
+			PlatformSellerPhoneNumber:          "NUMBER",
+			PlatformSellerPhoneAreaCode:        "11",
+			PlatformAuthentication:             "string",
+		},
 	}
+
 	resource, err := client.Create(context.Background(), request)
 	if err != nil {
 		fmt.Println(err)
