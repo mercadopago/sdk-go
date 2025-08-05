facc98a6d40f3923af4120f8df5a5a5b00e61d47
GianniTie <giannitie@gmail.com>
2025-08-05 16:01:06 -0300

    Add cbp support.

--- pkg/payment/request.go
+++ pkg/payment/request.go
@@ -16,6 +16,8 @@ type Request struct {
 	PaymentMethod      *PaymentMethodRequest      `json:"payment_method,omitempty"`
 	DateOfExpiration   *time.Time                 `json:"date_of_expiration,omitempty"`
 	Taxes              []TaxRequest               `json:"taxes,omitempty"`
+	Amounts            *AmountsRequest            `json:"amounts,omitempty"`
+	CounterCurrency    *CounterCurrencyRequest    `json:"counter_currency,omitempty"`
 
 	CallbackURL           string         `json:"callback_url,omitempty"`
 	CouponCode            string         `json:"coupon_code,omitempty"`
@@ -325,4 +327,21 @@ type TaxRequest struct {
 	Type       string  `json:"type,omitempty"`
 	Value      float64 `json:"value,omitempty"`
 	Percentage bool    `json:"percentage,omitempty"`
+}
+
+// AmountsRequest represents amounts request within Request.
+type AmountsRequest struct {
+	Collector UserAmountsRequest `json:"collector,omitempty"`
+	Payer     UserAmountsRequest `json:"payer,omitempty"`
+}
+
+// UserAmountsRequest represents user amounts request within AmountsRequest.
+type UserAmountsRequest struct {
+	CurrencyID  string  `json:"currency_id,omitempty"`
+	Transaction float64 `json:"transaction,omitempty"`
+}
+
+// CounterCurrencyRequest represents counter currency request within Request.
+type CounterCurrencyRequest struct {
+	CurrencyID string `json:"currency_id,omitempty"`
 }
